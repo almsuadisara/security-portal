3d9472fc43820fc028053a789bed3805e2f9ad19
Almsuadisara <almsudisara@gmail.com>
2024-11-10 12:37:32 +0300

    SP-9

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// connect to database
+//connect to database
 const connectDB = async () => {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
@@ -10,5 +10,5 @@ const connectDB = async () => {
 
     console.log(`MongoDB Connected: ${connection.connection.host}`);
 }; 
-
+// export
 module.exports = connectDB;
